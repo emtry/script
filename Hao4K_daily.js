2c10b602733d3d0e3102fc9c40010b2232a84b1d
ehgenong <pyzlypz@gmail.com>
2022-07-29 12:08:35 +0800

    Add files via upload

--- Hao4K_daily.js
+++ Hao4K_daily.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const iconv = require('iconv-lite');
+const notify = require('./sendNotify');
 
 // 自定义
 // Cookie
@@ -8,7 +9,7 @@ let Cookie = process.env.Hao4K_COOKIE || "";
 // User-Agent
 let UserAgent = process.env.Hao4K_UserAgent || "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.5 Safari/605.1.15";
 
-console.log("\nHao4K_sign\nVersion 1.0.0\n")
+console.log("\nHao4K_daily\nVersion 1.0.1\n")
 
 // 创建axios实例
 const axios_Hao4K = axios.create({
@@ -28,7 +29,8 @@ axios_Hao4K.get('/', {}).then(function(response) {
         let regex = />登录网站</;
         let m = regex.exec(iconv.decode(response.data, 'gbk'));
         if (m) {
-            console.log('未登录');
+            console.log('未登录\n');
+            notify.sendNotify('Hao4K_daily', '未登录');
         } else {
             // 判断是否已签到
             axios_Hao4K.get('/qiandao', {}).then(function(response) {
@@ -36,7 +38,8 @@ axios_Hao4K.get('/', {}).then(function(response) {
                     let regex = /您的签到排名/;
                     let m = regex.exec(iconv.decode(response.data, 'gbk'));
                     if (m) {
-                        console.log('今日已签到');
+                        console.log('今日已签到\n');
+                        notify.sendNotify('Hao4K_daily', '今日已签到');
                     } else {
                         regex = /id="JD_sign" href=\"(.*?)\" onclick=/gm;
                         m = regex.exec(iconv.decode(response.data, 'gbk'));
@@ -49,7 +52,8 @@ axios_Hao4K.get('/', {}).then(function(response) {
                                         let regex = /您的签到排名/;
                                         let m = regex.exec(iconv.decode(response.data, 'gbk'));
                                         if (m) {
-                                            console.log('签到成功');
+                                            console.log('签到成功\n');
+                                            notify.sendNotify('Hao4K_daily', '签到成功');
                                         }
                                     } else {
                                         console.log('未知错误');
